babdfa3d9a1a0be236e119a619db72a4dadb1b9e
Denis Kudinov <Denis.Kudinov@acronis.com>
2017-05-25 17:15:10 +0300

    * refactor admin client

--- admin_client/admin_client.go
+++ admin_client/admin_client.go
@@ -1,32 +1,43 @@
-package admin_info
+package admin_client
 
 import (
 	"google.golang.org/api/admin/directory/v1"
 	"golang.org/x/oauth2/google"
 	"io/ioutil"
 	"context"
+	"fmt"
 )
 
-func GetListOfUsers() (*admin.Users, error) {
+var service *admin.Service
+
+func Init() error {
 	ctx := context.Background()
 
 	b, err := ioutil.ReadFile("./Acronis-data-backup-58ecc97b43ae.json")
 	if err != nil {
-		return nil, err
+		return err
 	}
 
 	data, err := google.JWTConfigFromJSON(b, admin.AdminDirectoryUserScope, admin.AdminDirectoryUserReadonlyScope)// admin.AdminDirectoryUserReadonlyScope, "https://www.googleapis.com/auth/drive","https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive.readonly","https://www.googleapis.com/auth/drive.metadata.readonly","https://www.googleapis.com/auth/drive.metadata","https://www.googleapis.com/auth/drive.photos.readonly")
 	if err != nil {
-		return nil, err
+		return err
 	}
 
 	data.Subject = "admin@dkudinov.com"
 
-	adminService, err := admin.New(data.Client(ctx))
+	service, err = admin.New(data.Client(ctx))
 	if err != nil {
-		return nil, err
+		return err
 	}
-	usersListCall := adminService.Users.List()
+
+	return nil
+}
+
+func GetListOfUsers() (*admin.Users, error) {
+	if service == nil {
+		return nil, fmt.Errorf("Service not initialized. Call admin_client.Init() first.")
+	}
+	usersListCall := service.Users.List()
 	usersListCall = usersListCall.Domain("dkudinov.com")
 	usersListCall = usersListCall.Projection("full")
 
--- entrypoint.go
+++ entrypoint.go
@@ -1,12 +1,18 @@
 package main
 
 import (
-	"github.com/kudinovdenis/acronis-gd/admin_info"
+	"github.com/kudinovdenis/acronis-gd/admin_client"
 	"github.com/kudinovdenis/logger"
 )
 
 func main() {
-	users, err := admin_info.GetListOfUsers()
+	err := admin_client.Init()
+	if err != nil {
+		logger.Logf(logger.LogLevelDefault, "Cant initialize admin client. %s", err.Error())
+		return
+	}
+
+	users, err := admin_client.GetListOfUsers()
 	if err != nil {
 		logger.Logf(logger.LogLevelDefault, "Error while getting list of users: %s", err.Error())
 		return
