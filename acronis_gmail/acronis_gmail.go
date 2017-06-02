b9b71c04fb252b137b89d956f3e8117194fa17bc
Artem Olkov <artem.olkov@acronis.com>
2017-06-02 15:51:21 +0300

    + gmail API backup and restore

---
+++ acronis_gmail/acronis_gmail.go
@@ -0,0 +1,205 @@
+package acronis_gmail
+
+import (
+	"io/ioutil"
+	"google.golang.org/api/gmail/v1"
+	"context"
+	"golang.org/x/oauth2/google"
+	"github.com/kudinovdenis/logger"
+	"os"
+	"encoding/base64"
+)
+
+type GmailClient struct {
+	s *gmail.Service
+	currentUserID string
+}
+
+func Init(subject string) (*GmailClient, error) {
+	client := GmailClient{}
+	
+	ctx := context.Background()
+	
+	b, err := ioutil.ReadFile("./marketplace-test-app-fdd851a5bf90.json")
+	if err != nil {
+		return nil, err
+	}
+	
+	data, err := google.JWTConfigFromJSON(b,
+		gmail.MailGoogleComScope,
+		gmail.GmailComposeScope,
+		gmail.GmailInsertScope,
+		gmail.GmailLabelsScope,
+		gmail.GmailModifyScope,
+		gmail.GmailReadonlyScope,
+		gmail.GmailSendScope,
+		gmail.GmailSettingsBasicScope,
+		gmail.GmailSettingsSharingScope,
+	)
+	if err != nil {
+		logger.Logf(logger.LogLevelError, "JWT Config failed, %v", err)
+		return nil, err
+	}
+	
+	data.Subject = subject
+	
+	client.s, err = gmail.New(data.Client(ctx))
+	if err != nil {
+		logger.Logf(logger.LogLevelError, "New Gmail failed, %v", err)
+		return nil, err
+	}
+	
+	return &client, nil
+}
+
+func (c *GmailClient) Backup(account string) (err error){
+	
+	threads, err := c.s.Users.Threads.List(account).Do()
+	if err != nil {
+		logger.Logf(logger.LogLevelError, "Threads List failed , %v", err)
+		return
+	}
+	
+	logger.Logf(logger.LogLevelDefault, "%v", len(threads.Threads))
+	
+	for _, thread := range threads.Threads {
+		logger.Logf(logger.LogLevelDefault, "Started thread w/ ID : %v", thread.Id)
+		logger.Logf(logger.LogLevelDefault, "Thread snippet : %s ", thread.Snippet)
+		
+		path_to_backup := "./" + account + "/backup/" + string(thread.Id) + "/"
+		err = os.MkdirAll(path_to_backup, 0777)
+		if err != nil {
+			logger.Logf(logger.LogLevelError, "Directory create failed, %v", err)
+			return
+		}
+		
+		tc := c.s.Users.Threads.Get(account, thread.Id)//.Do()//service.Threads.Get(subject, thread.Id).Do()
+		tc = tc.Format("metadata")
+		t, err := tc.Do()
+		if err != nil {
+			logger.Logf(logger.LogLevelError, "Thread Get failed , %v", err)
+			return err
+		}
+		logger.Logf(logger.LogLevelDefault, "Getted Thread Snippet : %s", t.Snippet)
+		logger.Logf(logger.LogLevelDefault, "Getted Thread Message Count %v", len(t.Messages))
+		
+		for _, mes := range t.Messages {
+			logger.Logf(logger.LogLevelDefault, "Started message w/ ID : %v", mes.Id)
+			mc := c.s.Users.Messages.Get(account, mes.Id)
+			mc = mc.Format("raw")
+			m, err := mc.Do()
+			if err != nil {
+				logger.Logf(logger.LogLevelError, "Message Get failed , %v", err)
+				return err
+			}
+			logger.Logf(logger.LogLevelDefault, "Message snippet : %s", m.Snippet)
+			
+			raw_message := m.Raw
+			pb := path_to_backup + m.Id
+			
+			rm, _ := base64.URLEncoding.DecodeString(raw_message)
+			
+			err = ioutil.WriteFile(pb, rm , 0777)
+			if err != nil {
+				logger.Logf(logger.LogLevelError, "Write to File failed, %v", err)
+				return err
+			}
+			
+			//payload := m.Payload
+			//
+			//msg_part_string, err := payload.MarshalJSON()
+			//logger.Logf(logger.LogLevelDefault, "Message payload marshalled: %s", msg_part_string)
+			//
+			//logger.Logf(logger.LogLevelDefault, "Message payload mimetype : %s", payload.MimeType)
+			//
+			//for i, part := range payload.Parts {
+			//	logger.Logf(logger.LogLevelDefault, "Message payloadpart [%v] mimetype : %s", i, part.MimeType)
+			//	pr, _ := base64.URLEncoding.DecodeString(part.Body.Data)
+			//	logger.Logf(logger.LogLevelDefault, "Message payloadpart [%v] body: %s", i, pr)
+			//	//logger.Logf(logger.LogLevelDefault, "Message payloadpart [%v] body : %s", i, part.Body.Data)
+			//}
+			
+			logger.Logf(logger.LogLevelDefault, "Ended message w/ ID : %v", mes.Id)
+		}
+		logger.Logf(logger.LogLevelDefault, "Ended thread w/ ID : %v", thread.Id)
+	}
+	
+	return
+}
+
+func (c *GmailClient) Restore(account string, pathToBackup string) (err error) {
+	d, err := os.Open(pathToBackup)
+	if err != nil {
+		logger.Logf(logger.LogLevelError, "Directory open failed, %v", err)
+		return
+	}
+	defer d.Close()
+	fi, err := d.Readdir(-1)
+	if err != nil {
+		logger.Logf(logger.LogLevelError, "Directory open failed, %v", err)
+		return
+	}
+	for _, fi := range fi {
+		
+		if fi.IsDir() { //fi.Mode().IsRegular() {
+			logger.Logf(logger.LogLevelDefault, "Found dir: %v", fi.Name())
+			err = c.restoreThread(account, pathToBackup+fi.Name())
+			if err != nil {
+				logger.Logf(logger.LogLevelError, "Failed to restore thread if: %v, err: %v", fi.Name(), err.Error())
+			}
+		}
+	}
+	
+	
+	return
+}
+
+func (c *GmailClient) restoreThread(account string, pathToThread string) (err error) {
+	d, err := os.Open(pathToThread)
+	if err != nil {
+		logger.Logf(logger.LogLevelError, "Directory open failed, %v", err)
+		return
+	}
+	defer d.Close()
+	fi, err := d.Readdir(-1)
+	if err != nil {
+		logger.Logf(logger.LogLevelError, "Directory open failed, %v", err)
+		return
+	}
+	for _, fi := range fi {
+		
+		if !fi.IsDir() { //fi.Mode().IsRegular() {
+			logger.Logf(logger.LogLevelDefault, "Found file: %v", fi.Name())
+			err = c.restoreMessage(account, pathToThread+"/"+fi.Name())
+			if err != nil {
+				logger.Logf(logger.LogLevelError, "Failed to restore thread id: %v, err: %v", fi.Name(), err.Error())
+				return
+			}
+		}
+	}
+	
+	
+	return
+}
+
+func (c *GmailClient) restoreMessage(account string, pathToMsg string) (err error) {
+	raw, err := ioutil.ReadFile(pathToMsg)
+	if err != nil {
+		logger.Logf(logger.LogLevelError, "Failed to restore message path: %v, err: %v", pathToMsg, err.Error())
+		return
+	}
+	
+	var msg = gmail.Message{}
+	msg.Raw = base64.URLEncoding.EncodeToString(raw)
+	
+	ic := c.s.Users.Messages.Insert(account, &msg)
+	m, err := ic.Do()
+	if err != nil {
+		logger.Logf(logger.LogLevelError, "Failed to restore message path: %v, err: %v", pathToMsg, err.Error())
+		return err
+	}
+	
+	logger.Logf(logger.LogLevelDefault, "Inserted msg: %v", m)
+	
+	return
+}
