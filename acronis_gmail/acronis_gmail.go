ea85c71ce7dc683832fe24b2cf1760ab62973074
Alexander Gazarov <alexander.gazarov@acronis.com>
2017-06-05 12:28:48 +0300

    Reformatted the Gmail client code

--- acronis_gmail/acronis_gmail.go
+++ acronis_gmail/acronis_gmail.go
@@ -1,30 +1,31 @@
 package acronis_gmail
 
 import (
-	"io/ioutil"
-	"google.golang.org/api/gmail/v1"
 	"context"
-	"golang.org/x/oauth2/google"
-	"github.com/kudinovdenis/logger"
-	"os"
 	"encoding/json"
+	"io/ioutil"
+	"os"
+
+	"github.com/kudinovdenis/logger"
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/gmail/v1"
 )
 
 type GmailClient struct {
-	s *gmail.Service
+	s             *gmail.Service
 	currentUserID string
 }
 
 func Init(subject string) (*GmailClient, error) {
 	client := GmailClient{}
-	
+
 	ctx := context.Background()
-	
+
 	b, err := ioutil.ReadFile("./marketplace-test-app-fdd851a5bf90.json")
 	if err != nil {
 		return nil, err
 	}
-	
+
 	data, err := google.JWTConfigFromJSON(b,
 		gmail.MailGoogleComScope,
 		gmail.GmailComposeScope,
@@ -36,44 +37,45 @@ func Init(subject string) (*GmailClient, error) {
 		gmail.GmailSettingsBasicScope,
 		gmail.GmailSettingsSharingScope,
 	)
+
 	if err != nil {
 		logger.Logf(logger.LogLevelError, "JWT Config failed, %v", err)
 		return nil, err
 	}
-	
+
 	data.Subject = subject
-	
+
 	client.s, err = gmail.New(data.Client(ctx))
 	if err != nil {
 		logger.Logf(logger.LogLevelError, "New Gmail failed, %v", err)
 		return nil, err
 	}
-	
+
 	return &client, nil
 }
 
-func (c *GmailClient) Backup(account string) (err error){
-	
+func (c *GmailClient) Backup(account string) (err error) {
+
 	threads, err := c.s.Users.Threads.List(account).Do()
 	if err != nil {
 		logger.Logf(logger.LogLevelError, "Threads List failed , %v", err)
 		return
 	}
-	
+
 	logger.Logf(logger.LogLevelDefault, "%v", len(threads.Threads))
-	
+
 	for _, thread := range threads.Threads {
 		logger.Logf(logger.LogLevelDefault, "Started thread w/ ID : %v", thread.Id)
 		logger.Logf(logger.LogLevelDefault, "Thread snippet : %s ", thread.Snippet)
-		
-		path_to_backup := "./" + account + "/backup/" + string(thread.Id) + "/"
-		err = os.MkdirAll(path_to_backup, 0777)
+
+		pathToBackup := "./" + account + "/backup/" + string(thread.Id) + "/"
+		err = os.MkdirAll(pathToBackup, 0777)
 		if err != nil {
 			logger.Logf(logger.LogLevelError, "Directory create failed, %v", err)
 			return
 		}
-		
-		tc := c.s.Users.Threads.Get(account, thread.Id)//.Do()//service.Threads.Get(subject, thread.Id).Do()
+
+		tc := c.s.Users.Threads.Get(account, thread.Id) //.Do()//service.Threads.Get(subject, thread.Id).Do()
 		tc = tc.Format("metadata")
 		t, err := tc.Do()
 		if err != nil {
@@ -82,7 +84,7 @@ func (c *GmailClient) Backup(account string) (err error){
 		}
 		logger.Logf(logger.LogLevelDefault, "Getted Thread Snippet : %s", t.Snippet)
 		logger.Logf(logger.LogLevelDefault, "Getted Thread Message Count %v", len(t.Messages))
-		
+
 		for _, mes := range t.Messages {
 			logger.Logf(logger.LogLevelDefault, "Started message w/ ID : %v", mes.Id)
 			mc := c.s.Users.Messages.Get(account, mes.Id)
@@ -93,21 +95,21 @@ func (c *GmailClient) Backup(account string) (err error){
 				return err
 			}
 			logger.Logf(logger.LogLevelDefault, "Message snippet : %s", m.Snippet)
-			
-			marshalled, err := m.MarshalJSON()
-			pb := path_to_backup + m.Id
 
-			err = ioutil.WriteFile(pb, marshalled , 0777)
+			marshalled, err := m.MarshalJSON()
+			pb := pathToBackup + m.Id
+
+			err = ioutil.WriteFile(pb, marshalled, 0777)
 			if err != nil {
 				logger.Logf(logger.LogLevelError, "Write to File failed, %v", err)
 				return err
 			}
-			
+
 			logger.Logf(logger.LogLevelDefault, "Ended message w/ ID : %v", mes.Id)
 		}
 		logger.Logf(logger.LogLevelDefault, "Ended thread w/ ID : %v", thread.Id)
 	}
-	
+
 	return
 }
 
@@ -124,7 +126,7 @@ func (c *GmailClient) Restore(account string, pathToBackup string) (err error) {
 		return
 	}
 	for _, fi := range fi {
-		
+
 		if fi.IsDir() {
 			logger.Logf(logger.LogLevelDefault, "Found dir: %v", fi.Name())
 			err = c.restoreThread(account, pathToBackup+fi.Name())
@@ -133,8 +135,7 @@ func (c *GmailClient) Restore(account string, pathToBackup string) (err error) {
 			}
 		}
 	}
-	
-	
+
 	return
 }
 
@@ -151,7 +152,7 @@ func (c *GmailClient) restoreThread(account string, pathToThread string) (err er
 		return
 	}
 	for _, fi := range fi {
-		
+
 		if !fi.IsDir() {
 			logger.Logf(logger.LogLevelDefault, "Found file: %v", fi.Name())
 			err = c.restoreMessage(account, pathToThread+"/"+fi.Name())
@@ -161,8 +162,7 @@ func (c *GmailClient) restoreThread(account string, pathToThread string) (err er
 			}
 		}
 	}
-	
-	
+
 	return
 }
 
@@ -172,15 +172,15 @@ func (c *GmailClient) restoreMessage(account string, pathToMsg string) (err erro
 		logger.Logf(logger.LogLevelError, "Failed to restore message path: %v, err: %v", pathToMsg, err.Error())
 		return
 	}
-	
+
 	var msg = &gmail.Message{}
-	
+
 	err = json.Unmarshal(raw, msg)
 	if err != nil {
 		logger.Logf(logger.LogLevelError, "Failed to unmarshal message path: %v, err: %v", pathToMsg, err.Error())
 		return err
 	}
-	
+
 	var m = &gmail.Message{}
 	m.Raw = msg.Raw
 	m.Payload = msg.Payload
@@ -189,16 +189,16 @@ func (c *GmailClient) restoreMessage(account string, pathToMsg string) (err erro
 	m.Snippet = msg.Snippet
 	m.Header = msg.Header
 	m.InternalDate = msg.InternalDate
-	
+
 	ic := c.s.Users.Messages.Insert(account, m)
-	
+
 	res, err := ic.Do()
 	if err != nil {
 		logger.Logf(logger.LogLevelError, "Failed to restore message path: %v, err: %v", pathToMsg, err.Error())
 		return err
 	}
-	
+
 	logger.Logf(logger.LogLevelDefault, "Inserted msg: %v", res)
-	
+
 	return
 }
